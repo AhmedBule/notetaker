09e8637c2042b985c2e3404d4bbebb8639bb05d2
Ahmed Bule <ahmedbule01@gmail.com>
2020-06-14 16:31:47 -0500

    changed some routes and files

--- routes/apirroutes.js
+++ routes/apirroutes.js
@@ -4,8 +4,8 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var notesdb = require("../db/db.json");
+
 
 
 // ===============================================================================
@@ -39,14 +39,11 @@ module.exports = function(app) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+     
+      notesdb.push(req.body);
+      console.log(notesdb);
+      res.json(notesdb);
+    
   });
 
   // ---------------------------------------------------------------------------
